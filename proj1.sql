0ad5131b00354b414e2fd642d06e37705a45ceaf
shifangping <shifp9707@gmail.com>
2020-05-27 22:53:15 -0700

    q3

--- proj1.sql
+++ proj1.sql
@@ -56,31 +56,79 @@ AS
 -- Question 2ii
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, p.playerid, cp.schoolid, h.yearid
+  FROM people AS p 
+  INNER JOIN HallofFame AS h ON p.playerid = h.playerid
+  INNER JOIN CollegePlaying AS cp ON p.playerid = cp.playerid
+  WHERE inducted = 'Y' AND cp.schoolid IN (
+    SELECT s.schoolid
+    FROM Schools AS s
+    WHERE s.schoolstate = 'CA'
+  )
+  ORDER BY h.yearid DESC, schoolid ASC, p.playerid ASC
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, namefirst, namelast, cp.schoolid
+  FROM people AS p 
+  INNER JOIN HallofFame AS h ON p.playerid = h.playerid
+  LEFT JOIN CollegePlaying AS cp ON p.playerid = cp.playerid
+  WHERE inducted = 'Y'
+  ORDER BY p.playerid DESC, schoolid ASC
 ;
 
 -- Question 3i
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, namefirst, namelast, yearid, slg
+  FROM people AS p
+  INNER JOIN (
+    SELECT b.playerid, yearid, CAST((h - h2b - h3b - hr) + 2 * h2b + 3 * h3b + 4 * hr AS float) / CAST(ab AS float) AS slg
+    FROM batting AS b
+    WHERE ab > 50
+  ) AS s
+  ON p.playerid = s.playerid
+  ORDER BY slg DESC, yearid ASC, p.playerid ASC
+  LIMIT 10
 ;
 
 -- Question 3ii
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, namefirst, namelast, lslg
+  FROM people AS p
+  INNER JOIN (
+    SELECT playerid, CAST(SUM(h - h2b - h3b - hr) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr) AS float) / CAST(SUM(ab) AS float) AS lslg
+    FROM batting
+    GROUP BY playerid 
+    HAVING SUM(ab) > 50
+  ) AS s
+  ON p.playerid = s.playerid
+  ORDER BY lslg DESC, p.playerid ASC
+  LIMIT 10
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, lslg
+  FROM people AS p
+  INNER JOIN (
+    SELECT playerid, CAST(SUM(h - h2b - h3b - hr) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr) AS float) / CAST(SUM(ab) AS float) AS lslg
+    FROM batting
+    GROUP BY playerid 
+    HAVING SUM(ab) > 50
+  ) AS s
+  ON p.playerid = s.playerid
+  WHERE lslg > (
+    SELECT CAST(SUM(h - h2b - h3b - hr) + 2 * SUM(h2b) + 3 * SUM(h3b) + 4 * SUM(hr) AS float) / CAST(SUM(ab) AS float)
+    FROM batting
+    WHERE playerid = 'mayswi01'
+  )
+  ORDER BY lslg DESC
+
 ;
 
 -- Question 4i
